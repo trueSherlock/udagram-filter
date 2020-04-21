fb5577424ef969803985f763d3ef5954678683db
trueSherlock <trueSherlock@local>
2020-04-21 16:58:39 -0400

    Added cleanup code

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 import { log } from 'console';
+import { readdir } from 'fs';
 
 (async () => {
 
@@ -14,7 +15,6 @@ import { log } from 'console';
   // Use the body parser middleware for post requests
   //app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
@@ -30,21 +30,33 @@ import { log } from 'console';
 
   /**************************************************************************** */
 
-  //! END @TODO1
-
   app.get("/filteredimage",async(req,res)=>{
     var url:string = req.query.image_url;
     if(url){
-      const filteredPath = await filterImageFromURL(url);
+      const filteredPath:string = await filterImageFromURL(url);
       if(filteredPath){
         res.status(200).sendFile(filteredPath);
+        const tmp_dir:string = __dirname+"/util/tmp";
+        
+        readdir(tmp_dir,(err,files)=>{
+          if(err){
+            console.log("Error reading tmp dir : "+tmp_dir);
+          }else{
+            try{
+              deleteLocalFiles(files,tmp_dir);
+              console.log(`Cleared tmp folder! Removed ${files.length} files`);
+            }catch(e){
+              console.log("Error deleting files :"+e);
+            }
+          }
+        });
       }else{
-        res.status(422).send("No valid image found at URL:"+url);
+        res.status(422).send("No valid image found at URL ==> "+url);
       }
     }else{
       res.status(400).send("image_url is required");
     }
-  })
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
